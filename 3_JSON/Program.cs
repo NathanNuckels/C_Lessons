f2a7dc636cab405a0c2cdede93fa4e77e4011ab5
NathanNuckels <48299408+NathanNuckels@users.noreply.github.com>
2021-06-22 18:24:02 -0500

    Added proj 3

---
+++ 3_JSON/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3_JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
