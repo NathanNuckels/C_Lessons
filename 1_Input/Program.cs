21e9b64008386818c307058c16d117e5f6cf81be
NathanNuckels <48299408+NathanNuckels@users.noreply.github.com>
2021-06-22 17:18:25 -0500

    Finished Project1 and created 2

--- 1_Input/Program.cs
+++ 1_Input/Program.cs
@@ -4,9 +4,25 @@ namespace _1_Input
 {
     class Program
     {
+        static string playerName;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("----- GAME START -----");
+            Console.WriteLine();
+            Console.WriteLine("Hello! Whats your name?");
+            playerName = Console.ReadLine();
+            Console.WriteLine($"Hello, {playerName}!");
+            Console.WriteLine("Want to save your progress? [y/n]");
+            string choice = Console.ReadLine().ToLower();
+            if (choice == "y" || choice == "yes")
+            {
+                Console.WriteLine("Too bad! idk how to write a file in C#!");
+                Console.WriteLine("...yet");
+            }
+            else
+            {
+                Console.WriteLine("Oh ok. Im not sure how to do that anyway.");
+            }
         }
     }
 }
